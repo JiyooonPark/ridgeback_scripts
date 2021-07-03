3c0c397303c40b416842ec776c991bfbb8657e0c
jypark <jiyoonpark0207@gmail.com>
2021-07-03 19:22:05 +0900

    Update laser_scan_right.py
    use different angles for real life | simulation

--- src/laser_scan_right.py
+++ src/laser_scan_right.py
@@ -15,9 +15,14 @@ def callback_laser(msg):
     # print ("90:{:.4}".format(msg.ranges[360]))
     # print ("180:{:.4}".format(msg.ranges[719]))
     # print()
-    angle_60 = (msg.ranges[490]+msg.ranges[500]+msg.ranges[510])/3
-    angle_90 = (msg.ranges[530]+msg.ranges[540]+msg.ranges[550])/3
-    angle_120 = (msg.ranges[570]+msg.ranges[580]+msg.ranges[590])/3
+    # for real ridgeback 
+    # angle_60 = (msg.ranges[490]+msg.ranges[500]+msg.ranges[510])/3
+    # angle_90 = (msg.ranges[530]+msg.ranges[540]+msg.ranges[550])/3
+    # angle_120 = (msg.ranges[570]+msg.ranges[580]+msg.ranges[590])/3
+    # for simulation
+    angle_60 = (msg.ranges[230]+msg.ranges[240]+msg.ranges[250])/3
+    angle_90 = (msg.ranges[350]+msg.ranges[360]+msg.ranges[370])/3
+    angle_120 = (msg.ranges[470]+msg.ranges[480]+msg.ranges[490])/3
     m = math.sqrt(3)/2
 
     # print("{:.4} {:.4} {:.4}".format(angle_120, angle_90, angle_60))
