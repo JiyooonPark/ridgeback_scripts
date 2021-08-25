4747e3194911e252615676ab3f9ed7a8d6dbb49f
jypark <jiyoonpark0207@gmail.com>
2021-08-25 23:15:27 +0900

    update follow trajectory

--- src/cmd_vel_follow_trajectory.py
+++ src/cmd_vel_follow_trajectory.py
@@ -70,10 +70,15 @@ def follow_traj(path):
 
 
 def f(xx):
-    x_points = [-4, -3, -2, -1, 0, 1, 2, 3, 4]
-    y_points = [-6, 1, 2, 4, -1, 4, 0, -1, -4]
+    # x_points = [9.96, 9.94, 9.51, 8.68, 8.24, 7.98, 6.48, 6.06, 5.02, 4.6, 4.27, 3.36, 2.62, 1.71, 0.46, 0.38, -0.07, -0.94, -1.21, -2.67, -3.24, -3.74]
+    # y_points = [-7.28, -7.19, -5.77, -5.17, -3.75, -3.0, -3.33, -4.39, -5.42, -6.45, -5.19, -3.97, -4.27, -4.59, -5.8, -5.05, -6.34, -5.69, -5.47, -5.37, -5.97, -4.73]
 
-    tck = interpolate.splrep(x_points, y_points)
+    x_points = [-3.08, -1.39, -0.18, 0.08, 1.73, 1.84, 3.78, 4.51, 4.69, 6.04, 6.84, 8.16, 8.78, 9.24, 10.05]
+    y_points = [-5.66, -4.98, -6.02, -5.44, -4.3, -4.32, -4.03, -6.07, -5.22, -4.1, -2.57, -3.06, -5.01, -5.22, -6.93]
+
+    y_positive = [abs(y) for y in y_points]
+
+    tck = interpolate.splrep(x_points, y_positive)
     return interpolate.splev(xx, tck)
 
 
@@ -83,7 +88,7 @@ if __name__ == '__main__':
         amcl_sub = rospy.Subscriber(
             '/amcl_pose', PoseWithCovarianceStamped, get_current_position)
         rospy.sleep(0.5)
-        path_x = [0.4 * n for n in range(-10, 10)]
+        path_x = [0.4 * n for n in range(-5, 12)]
         path_y = [float(f(n)) for n in path_x]
         path = zip(path_x, path_y)
         follow_traj(path)
