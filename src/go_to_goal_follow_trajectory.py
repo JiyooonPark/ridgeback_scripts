e8d6ec534fafaa04ec37a28c29e498ffec6e9218
jypark <jiyoonpark0207@gmail.com>
2021-09-06 18:50:23 +0900

    ready to clean repo

--- src/go_to_goal_follow_trajectory.py
+++ src/go_to_goal_follow_trajectory.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python
-
+#!/usr/bin/env python3
 
 import rospy
 import actionlib
@@ -7,20 +6,9 @@ from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseWithCovarianceStamped
 
-init_x = -100
-init_y = 0
-init_z = 0
-init_w = 0
-
 
 def callback(msg):
-    global init_x, init_y, init_z, init_w
-    if init_x == -100:
-        init_x = msg.pose.pose.orientation.x
-        init_y = msg.pose.pose.orientation.y
-        init_z = msg.pose.pose.orientation.z
-        init_w = msg.pose.pose.orientation.w
-
+    pass
 
 def movebase_client(goal_x, goal_y):
 
@@ -32,10 +20,10 @@ def movebase_client(goal_x, goal_y):
     goal.target_pose.header.stamp = rospy.Time.now()
     goal.target_pose.pose.position.x = goal_x
     goal.target_pose.pose.position.y = goal_y
-    # goal.target_pose.pose.orientation.x = 0
-    # goal.target_pose.pose.orientation.y = 0
-    # goal.target_pose.pose.orientation.z = 0
-    # goal.target_pose.pose.orientation.w = 0.5
+    goal.target_pose.pose.orientation.x = 0
+    goal.target_pose.pose.orientation.y = 0
+    goal.target_pose.pose.orientation.z = 0
+    goal.target_pose.pose.orientation.w = 1
 
     client.send_goal(goal)
     wait = client.wait_for_result()
@@ -51,14 +39,15 @@ if __name__ == "__main__":
     try:
         rospy.init_node("movebase_client_py")
         odom_sub = rospy.Subscriber("/amcl_pose", PoseWithCovarianceStamped, callback)
-        x_points = [-3.08, -1.61, -0.24, 0.11, 1.81, 3.72, 4.51, 4.69, 6.04, 6.84, 8.16, 8.78, 9.15, 10.05]
-        y_points = [-5.66, -4.93, -6.1, -5.54, -4.42, -4.03, -6.07, -5.22, -4.1, -2.57, -3.06, -5.01, -5.23, -6.93]
-        y_positive = [abs(y)-0.5 for y in y_points]
+        x_points = [-1.65, -1.25, -0.88, -0.34, 0.13, 0.64, 1.19, 1.78, 2.0, 2.58, 3.05, 3.39, 3.96, 4.33, 4.74, 5.24, 5.86, 6.09, 6.36, 6.76, 7.29, 7.72, 8.21, 8.88, 9.02, 9.49, 9.96]
+        y_points = [-4.75, -4.99, -5.19, -5.2, -5.11, -5.02, -5.0, -5.1, -5.16, -5.28, -5.25, -5.11, -4.76, -4.47, -4.37, -4.39, -4.76, -5.38, -5.53, -5.54, -5.41, -5.15, -5.02, -5.08, -5.14, -5.59, -5.75]
+        y_positive = [abs(y)-1.5 for y in y_points]
         for (x, y) in zip(x_points, y_positive):
+            print(x, y)
 
-            result = movebase_client(x, y)
+            result = movebase_client(x + 1, y)
+            
             if result:
-                print (init_x, init_y, init_z, init_w)
                 rospy.loginfo("Goal execution done!")
 
     except rospy.ROSInterruptException:
